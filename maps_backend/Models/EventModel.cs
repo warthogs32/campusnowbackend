68929e606da8d99ea8a5b198839215c2e4ebca56
jason-e6530 <jason@kefi-foundation.org>
2020-02-04 20:57:04 -0800

    removed views

---
+++ maps_backend/Models/EventModel.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace maps_backend.Models
+{
+    public class EventModel
+    {
+    }
+}
---
+++ maps_backend/Repositories/EventRepository.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace maps_backend.Repositories
+{
+    public class EventRepository
+    {
+    }
+}
