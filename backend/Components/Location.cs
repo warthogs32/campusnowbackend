b097a28a44d9006ec9f2bc96652bb8e8b729f0c9
jason-e6530 <jason@kefi-foundation.org>
2020-02-09 22:23:03 -0800

    remove views
    add events model and controller

---
+++ backend/Components/Location.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace backend.Components
+{
+    public class Location
+    {
+        private double _x;
+        public double X
+        {
+            get => _x;
+            set
+            {
+                _x = value;
+            }
+        }
+
+        private double _y;
+        public double Y
+        {
+            get => _y;
+            set
+            {
+                _y = value;
+            }
+        }
+    }
+}
