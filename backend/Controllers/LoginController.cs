c48c716314bc6143e710a1b00bc6843ca18e9239
jason-e6530 <jason@kefi-foundation.org>
2020-02-19 14:34:00 -0800

    Update LoginController.cs
    simplify statements

--- backend/Controllers/LoginController.cs
+++ backend/Controllers/LoginController.cs
@@ -76,8 +76,8 @@ namespace backend.Controllers
 
             const string sec = "401b09eab3c013d4ca54922bb802bec8fd5318192b0a75f201d8b3727429090fb337591abd3e44453b954555b7a0812e1081c39b740293f765eae731f5a65ed1";
             var now = DateTime.UtcNow;
-            var securityKey = new Microsoft.IdentityModel.Tokens.SymmetricSecurityKey(System.Text.Encoding.Default.GetBytes(sec));
-            var signingCredentials = new Microsoft.IdentityModel.Tokens.SigningCredentials(securityKey, Microsoft.IdentityModel.Tokens.SecurityAlgorithms.HmacSha256Signature);
+            var securityKey = new SymmetricSecurityKey(System.Text.Encoding.Default.GetBytes(sec));
+            var signingCredentials = new SigningCredentials(securityKey, SecurityAlgorithms.HmacSha256Signature);
 
 
             //create the jwt
