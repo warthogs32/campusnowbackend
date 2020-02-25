f031a813ce2dd21b0a7bf5f812f43d435927cb0d
Hunter Lane <hunterlane6299@gmail.com>
2020-02-24 22:10:44 -0800

    Update EventsTest.cs
    Added starter unit tests for #59 #60 and #74. Still need to setup expected values and login testing

--- backend.Tests/EventsTest.cs
+++ backend.Tests/EventsTest.cs
@@ -70,5 +70,69 @@ namespace backend.Tests
             // Assert
             Assert.IsTrue(result);
         }
+
+        [TestMethod]
+        public void TestUpdateEvent()
+        {
+            // Arrange
+            LoginRepository loginRepo = new LoginRepository();
+            EventRepository repo = new EventRepository();
+            EventRecord record = new EventRecord()
+            {
+                Title = "Test Update Event",
+                Description = "Test Update Description",
+                StartTime = new DateTime(2020, 3, 25, 1, 0, 0),
+                EndTime = new DateTime(2020, 3, 25, 2, 15, 0),
+                LocX = 30.35,
+                LocY = 120.74,
+                UserId = 1
+            };
+
+            // Act
+            bool result = repo.UpdateEvent(record);
+
+            // Assert
+            Assert.IsTrue(result);
+        }
+
+        [TestMethod]
+        public void TestDeleteEvent()
+        {
+            // Arrange
+            EventRepository repo = new EventRepository();
+            int id = 3;
+
+            // Act
+            bool result = repo.DeleteEvent(id);
+
+            // Assert
+            Assert.IsTrue(result);
+        }
+
+        [TestMethod]
+        public void TestGetEventsByTimeRange()
+        {
+            // Arrange
+            EventRepository repo = new EventRepository();
+            DateTime start = new DateTime(2020, 3, 1, 1, 0, 0);
+            DateTime finish = new DateTime(2020, 4, 1, 1, 0, 0);
+            List<EventRecord> expectedRecords = new List<EventRecord>();
+            //TODO: build expected list of eventRecords
+            int expectedSize = expectedRecords.Count;
+
+            // Act
+            List<EventRecord> results = repo.GetEventsByTimeRange(start, finish);
+
+            // Assert
+            Assert.AreEqual(results.Count, expectedSize);
+            for (int i = 0; i < expectedSize; i++)
+            {
+                Assert.AreEqual(results[i].ListingId, expectedRecords[i].ListingId);
+                Assert.AreEqual(results[i].Title, expectedRecords[i].Title);
+                Assert.AreEqual(results[i].Description, expectedRecords[i].Description);
+                Assert.AreEqual(results[i].StartTime, expectedRecords[i].StartTime);
+                Assert.AreEqual(results[i].EndTime, expectedRecords[i].EndTime);
+            }
+        }
     }
 }
